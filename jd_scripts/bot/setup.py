4c01e1c46430642f420bd3d69b9c8e30e08b6399
Akira-Work <akira-work@Akira-MacBook-Pro.local>
2020-11-04 13:04:43 +0800

    jd_scripts镜像增加telegram bot 支持

---
+++ jd_scripts/bot/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author    : iouAkira(lof)
+# @mail      : e.akimoto.akira@gmail.com
+# @CreateTime: 2020-11-02
+# @UpdateTime: 2020-11-03
+
+from setuptools import setup
+
+setup(
+    name='jd-scripts-bot',
+    version='0.1',
+    scripts=['jd_bot', ],
+)
