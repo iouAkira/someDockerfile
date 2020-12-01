9fd95cfc976697c24e585034886b81b42a5f8400
Akira <e.akimoto.akira@gmail.com>
2020-12-01 22:48:07 +0800

    Update cronlist.sh

--- pack_some_script/cronlist.sh
+++ pack_some_script/cronlist.sh
@@ -1,2 +1,2 @@
 */30 * * * * cd /xmly_speed && python3 xmly_speed.py |ts >> /logs/xmly_speed.log 2>&1
-*/11 * * * * cd /qeyd/Script && node qeyd_replace.js |ts >> /logs/qeyd.log 2>&1
+*/11 * * * * cd /qeyd/Scripts && node qeyd_replace.js |ts >> /logs/qeyd.log 2>&1
