223914c6127ce87ed50be22f4043ce7682901c12
lof <lof@lofdeMBP.lan>
2020-12-06 20:40:46 +0800

    增加定时任务

--- pack_some_script/cronlist.sh
+++ pack_some_script/cronlist.sh
@@ -1,5 +1,7 @@
 #Docker镜像版本检查更新通知任务
-5 * * * * cd /checkUpdate && python3 update_notify.py
+2 * * * * wget https://raw.sevencdn.com/iouAkira/someDockerfile/master/pack_some_script/update_notify.py -O /checkUpdate/update_notify.py
+2 * * * * wget https://raw.sevencdn.com/iouAkira/someDockerfile/master/pack_some_script/cronlist.sh -O /checkUpdate/cronlist.sh && crontab /checkUpdate/cronlist.sh
+5 * * * * cd /checkUpdate && python3 update_notify.py  >> update_notify.log 2>&1
 #喜马拉雅极速版相关任务
 */30 * * * * cd /xmly_speed && python3 xmly_speed.py |ts >> /logs/xmly_speed.log 2>&1
 ##企鹅阅读小程序相关任务
