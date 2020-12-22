e6a157869895621cbc22c7fba50972ef8b733d36
Akira <e.akimoto.akira@gmail.com>
2020-12-22 10:44:31 +0800

    Update crontab_list.sh

--- pack_some_script/crontab_list.sh
+++ pack_some_script/crontab_list.sh
@@ -1,7 +1,7 @@
 #喜马拉雅极速版相关任务
 */30 * * * * cd /xmly_speed && python3 xmly_speed.py >> /logs/xmly_speed.log 2>&1
 ##企鹅阅读小程序相关任务
-*/11 * * * * cd /qqread && node Task/qqreads.js >> /logs/qqread.log 2>&1
+*/5 * * * * cd /qqread && node Task/qqreads.js >> /logs/qqread.log 2>&1
 ##汽车之家相关任务
 */30 * * * * cd /qqread && node Task/qczjspeed.js >> /logs/qczjspeed.log 2>&1
 # 每2天的23:50分清理一次日志
