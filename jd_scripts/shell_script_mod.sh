9a8e90f7871862b4cace9a0e06efd0c62fc4fd80
Akira <e.akimoto.akira@gmail.com>
2021-01-02 22:29:39 +0800

    Create shell_script_mod.sh

---
+++ jd_scripts/shell_script_mod.sh
@@ -0,0 +1,5 @@
+##下面只是做一个示例
+##使用自定义shell下载并配置执行农场
+wget -O /scripts/jx_nc.js https://raw.githubusercontent.com/whyour/hundun/master/quanx/jx_nc.js
+echo -e >>$mergedListFile
+echo "10 9,18 * * * node /scripts/jx_nc.js |ts >> /scripts/logs/jx_nc.log 2>&1" >>$mergedListFile
