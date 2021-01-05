f00ab43764059b8e8f8c20af0a90db209386d67a
Akira <e.akimoto.akira@gmail.com>
2021-01-05 21:52:25 +0800

    Update shell_script_mod.sh

--- jd_scripts/shell_script_mod.sh
+++ jd_scripts/shell_script_mod.sh
@@ -5,5 +5,5 @@ echo -e >> /scripts/docker/merged_list_file.sh
 echo "10 9,18 * * * node /scripts/jx_nc.js |ts >> /scripts/logs/jx_nc.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 #临时增加红包雨
-wget -O /scripts/jd_live_redrain.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_live_redrain.js
+echo "28,29 20-23/1 * * * wget -O /scripts/jd_live_redrain.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_live_redrain.js " >> /scripts/docker/merged_list_file.sh
 echo "30,31 20-23/1 * * * node /scripts/jd_live_redrain.js |ts >> /scripts/logs/jd_live_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
