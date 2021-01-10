7e5279b8bf2e9e09b848dfdd84fbfa39cca5c6e2
Akira <e.akimoto.akira@gmail.com>
2021-01-10 21:18:09 +0800

    Update shell_script_mod.sh

--- jd_scripts/shell_script_mod.sh
+++ jd_scripts/shell_script_mod.sh
@@ -1,3 +1,5 @@
+# !/bin/sh
+
 ##下面只是做一个示例
 ##使用自定义shell下载并配置执行农场
 # wget -O /scripts/jx_nc.js https://raw.githubusercontent.com/whyour/hundun/master/quanx/jx_nc.js
@@ -7,3 +9,6 @@
 # #临时增加红包雨
 # echo "28,29 20-23/1 * * * wget -O /scripts/jd_live_redrain.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_live_redrain.js " >> /scripts/docker/merged_list_file.sh
 # echo "30,31 20-23/1 * * * node /scripts/jd_live_redrain.js |ts >> /scripts/logs/jd_live_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
+
+#由于作者还没有增加互助码环境变量，就自己手动sed进去了
+sed -i "s/shareCodesArr = []/shareCodesArr = ['a25442c9de1a47ddbe4cd3c4828bd8ea@aba172068b7a46e2b6cf89563b919053','ed23af1e5a5946b381266b2192f8d4a2@aba172068b7a46e2b6cf89563b919053','ed23af1e5a5946b381266b2192f8d4a2@a25442c9de1a47ddbe4cd3c4828bd8ea']/g" /scripts/jd_bookshop.js
