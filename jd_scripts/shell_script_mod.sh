32e81ad7dc100bb41eef8e2d810edf5fb23a43a8
Akira <e.akimoto.akira@gmail.com>
2021-01-12 10:05:23 +0800

    Update shell_script_mod.sh

--- jd_scripts/shell_script_mod.sh
+++ jd_scripts/shell_script_mod.sh
@@ -13,7 +13,7 @@
 ##使用自定义shell下载并配置执行美妆
 wget -O /scripts/jd_mh.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_mh.js
 echo -e >> /scripts/docker/merged_list_file.sh
-echo "10 8,9,10 * * * node /scripts/jx_nc.js |ts >> /scripts/logs/jd_mh.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "10 8,9,10 * * * node /scripts/jd_mh.js |ts >> /scripts/logs/jd_mh.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 
 #由于bookshop作者还没有增加互助码环境变量，就自己手动sed进去了
