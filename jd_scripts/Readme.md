e771ed6ca8e9c7623506a17f26cb9724a0a9e235
Akira <e.akimoto.akira@gmail.com>
2021-01-16 12:10:09 +0800

    调整精简脚本

--- jd_scripts/Readme.md
+++ jd_scripts/Readme.md
@@ -1,2 +1,5 @@
-![Docker Pulls](https://img.shields.io/docker/pulls/akyakya/jd_scripts?style=for-the-badge) 
-## 具体是使用说明[_点这里_](https://github.com/lxk0301/scripts/tree/master/docker#usage)
+## 自用不提供任何使用咨询和帮助，如要使用自行查看dockerfile和对应的shell脚本逻辑
+## 自用不提供任何使用咨询和帮助，如要使用自行查看dockerfile和对应的shell脚本逻辑
+## 自用不提供任何使用咨询和帮助，如要使用自行查看dockerfile和对应的shell脚本逻辑
+
+## 能读懂逻辑的可转载修改搬运PR至原作者@lxk0301的gitee仓库
---
+++ jd_scripts/task_shell_script.sh
@@ -0,0 +1,136 @@
+#!/bin/sh
+set -e
+
+# 取消镜像自动通知
+# ######################################获取docker构建文件里面的自定义信息方法-start#####################################################
+# function getDockerImageLabel() {
+#     repo=akyakya/jd_scripts
+#     imageTag=latest
+#     token=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${repo}:pull" | jq -r '.token')
+#     digest=$(curl -s -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -H "Authorization: Bearer $token" "https://registry-1.docker.io/v2/${repo}/manifests/${imageTag}" | jq .config.digest -r)
+#     labels=$(curl -s -L -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -H "Authorization: Bearer $token" "https://registry-1.docker.io/v2/${repo}/blobs/$digest" | jq .config.Labels)
+#     echo $labels
+# }
+# ######################################获取docker构建文件里面的自定义信息方法-end#####################################################
+
+# ######################################对比版本版本号大小方法-start###################################################################
+# function version_gt() {
+#     test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"
+# }
+# ######################################对比版本版本号大小方法-end###################################################################
+
+# #######################################通知用户更新镜像-start#####################################################################
+# echo "check docker images update..."
+# echo "检查docker镜像更新更新..."
+# if type jq >/dev/null 2>&1; then
+#     echo "get dockerhub repo images labels..."
+#     echo "获取dockerhub仓库镜像labels信息..."
+#     labels=$(getDockerImageLabel)
+#     export NOTIFY_CONTENT=$(echo $labels | jq -r .UPDATE_CONTENT)
+#     version=$(echo $labels | jq -r .VERSION)
+# else
+#     # 第一版通知逻辑无法包含在上面判断里面，镜像构建好直接开启通知
+#     echo "Current container version is too old, send update notification"
+#     echo "当前版本过旧，发送镜像更新通知"
+#     export NOTIFY_CONTENT="更新内容较多，重新阅读仓库Readme(https://github.com/lxk0301/jd_scripts/tree/master/docker)，更新镜像并更新配置后使用。"
+#     cd /scripts/docker
+#     node notify_docker_user.js
+# fi
+
+# #通知通知用户更新镜像
+# if [ ! $BUILD_VERSION ]; then
+#     if [ $version ]; then
+#         echo "Current container version is empty, dockerhub lastet $version, send update notification"
+#         echo "当前容器版本为空，dockerhub仓库版本为$version，发送更新通知"
+#         cd /scripts/docker
+#         node notify_docker_user.js
+#     fi
+# else
+#     if version_gt $version $BUILD_VERSION; then
+#         echo "Current container version $BUILD_VERSION, dockerhub lastet version $version, send update notification"
+#         echo "当前容器版本为$BUILD_VERSION，dockerhub仓库版本为$version，发送通知"
+#         cd /scripts/docker
+#         node notify_docker_user.js
+#     fi
+# fi
+# #######################################通知用户更新镜像-end#####################################################################
+
+
+echo "定义定时任务合并处理用到的文件路径..."
+defaultListFile="/scripts/docker/$DEFAULT_LIST_FILE"
+echo "默认文件定时任务文件路径为 ${defaultListFile}"
+if [ $CUSTOM_LIST_FILE ]; then
+    customListFile="/scripts/docker/$CUSTOM_LIST_FILE"
+    echo "自定义定时任务文件路径为 ${customListFile}"
+fi
+mergedListFile="/scripts/docker/merged_list_file.sh"
+echo "合并后定时任务文件路径为 ${mergedListFile}"
+
+echo "第3步将默认定时任务列表添加到并后定时任务文件..."
+cat $defaultListFile >$mergedListFile
+
+echo "第2步判断是否存在自定义任务任务列表并追加..."
+if [ $CUSTOM_LIST_FILE ]; then
+    echo "您配置了自定义任务文件：$CUSTOM_LIST_FILE，自定义任务类型为：$CUSTOM_LIST_MERGE_TYPE..."
+    if [ -f "$customListFile" ]; then
+        if [ $CUSTOM_LIST_MERGE_TYPE == "append" ]; then
+            echo "合并默认定时任务文件：$DEFAULT_LIST_FILE 和 自定义定时任务文件：$CUSTOM_LIST_FILE"
+            echo -e "" >>$mergedListFile
+            cat $customListFile >>$mergedListFile
+        elif [ $CUSTOM_LIST_MERGE_TYPE == "overwrite" ]; then
+            echo "配置了自定义任务文件：$CUSTOM_LIST_FILE，自定义任务类型为：$CUSTOM_LIST_MERGE_TYPE..."
+            cat $customListFile >$mergedListFile
+        else
+            echo "配置配置了错误的自定义定时任务类型：$CUSTOM_LIST_MERGE_TYPE，自定义任务类型为只能为append或者overwrite..."
+        fi
+    else
+        echo "配置的自定义任务文件：$CUSTOM_LIST_FILE未找到，使用默认配置$DEFAULT_LIST_FILE..."
+    fi
+else
+    echo "当前只使用了默认定时任务文件 $DEFAULT_LIST_FILE ..."
+fi
+
+echo "第3步判断是否配置了默认脚本更新任务..."
+if [ $(grep -c "default_task.sh" $mergedListFile) -eq '0' ]; then
+    echo "合并后的定时任务文件，未包含必须的默认定时任务，增加默认定时任务..."
+    echo -e >>$mergedListFile
+    echo "52 */1 * * * docker_entrypoint.sh >> /scripts/logs/default_task.log 2>&1" >>$mergedListFile
+else
+    sed -i 'default_task.sh/d' $mergedListFile
+    echo "52 */1 * * * docker_entrypoint.sh >> /scripts/logs/default_task.log 2>&1" >>$mergedListFile
+fi
+
+echo "第5步判断是否配置了随即延迟参数..."
+if [ $RANDOM_DELAY_MAX ]; then
+    if [ $RANDOM_DELAY_MAX -ge 1 ]; then
+        echo "已设置随机延迟为 $RANDOM_DELAY_MAX , 设置延迟任务中..."
+        sed -i "/\(jd_bean_sign.js\|jd_blueCoin.js\|jd_joy_reward.js\|jd_joy_steal.js\|jd_joy_feedPets.js\|jd_car_exchange.js\)/!s/node/sleep \$((RANDOM % \$RANDOM_DELAY_MAX)); node/g" $mergedListFile
+    fi
+else
+    echo "未配置随即延迟对应的环境变量，故不设置延迟任务..."
+fi
+
+
+echo "第6步判断是否配置自定义shell执行脚本..."
+if [ 0"$CUSTOM_SHELL_FILE" = "0" ]; then
+    echo "未配置自定shell脚本文件，跳过执行。"
+else
+    if expr "$CUSTOM_SHELL_FILE" : 'http.*' &>/dev/null; then
+        echo "自定义shell脚本为远程脚本，开始下在自定义远程脚本。"
+        wget -O /jds/shell_script_mod.sh $CUSTOM_SHELL_FILE
+        echo "下载完成，开始执行..."
+        sh -x /jds/shell_script_mod.sh
+        echo "自定义远程shell脚本下载并执行结束。"
+    else
+        if [ ! -f $CUSTOM_SHELL_FILE ]; then
+            echo "自定义shell脚本为docker挂载脚本文件，但是指定挂载文件不存在，跳过执行。"
+        else
+            echo "docker挂载的自定shell脚本，开始执行..."
+            sh -x $CUSTOM_SHELL_FILE
+            echo "docker挂载的自定shell脚本，执行结束。"
+        fi
+    fi
+fi
+
+echo "第7步增加 |ts 任务日志输出时间戳..."
+sed -i "/\(ts\| ts\)/!s/>>/|ts >>/g" $mergedListFile
