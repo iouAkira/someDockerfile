9eaf7b6ae9d0f161e8e68ec86876bc561333daf3
Akira <e.akimoto.akira@gmail.com>
2021-01-16 13:48:03 +0800

    调整cmd参数判断

--- jd_scripts/docker_entrypoint.sh
+++ jd_scripts/docker_entrypoint.sh
@@ -4,13 +4,12 @@ set -e
 #获取配置的自定义参数
 if [ $1 ]; then
     run_cmd=$1
-else
-    run_cmd="crond"
 fi
 
 echo "设定远程仓库地址..."
 cd /scripts
 git remote set-url origin $REPO_URL
+git reset --hard
 echo "git pull拉取最新代码..."
 git -C /scripts pull
 echo "npm install 安装最新依赖"
@@ -24,15 +23,20 @@ echo "------------------------------------------------执行定时任务任务sh
 sh -x /jds/jd_scripts/task_shell_script.sh
 echo "--------------------------------------------------默认定时任务执行完成---------------------------------------------------"
 
-if [ $run_cmd == 'jd_bot' ]; then
-    echo "Start crontab task main process..."
-    echo "启动crondtab定时任务主进程..."
-    crond
-    echo "Start crontab task main process..."
-    echo "启动jd_bot..."
-    jd_bot
+if [ $run_cmd ]; then
+    if [ $run_cmd == 'jd_bot' ]; then
+        echo "Start crontab task main process..."
+        echo "启动crondtab定时任务主进程..."
+        crond
+        echo "Start crontab task main process..."
+        echo "启动jd_bot..."
+        jd_bot
+    fi
+    if [ $run_cmd == 'crond' ]; then
+        echo "Start crontab task main process..."
+        echo "启动crondtab定时任务主进程..."
+        crond -f
+    fi
 else
-    echo "Start crontab task main process..."
-    echo "启动crondtab定时任务主进程..."
-    crond -f
+    echo "默认定时任务执行结束。"
 fi
