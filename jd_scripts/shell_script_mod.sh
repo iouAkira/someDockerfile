4a0da28e0013802a15c10497b89e91ba78d04f3f
Akira <e.akimoto.akira@gmail.com>
2021-01-17 20:09:52 +0800

    Update shell_script_mod.sh

--- jd_scripts/shell_script_mod.sh
+++ jd_scripts/shell_script_mod.sh
@@ -7,8 +7,8 @@
 # echo "10 9,18 * * * node /scripts/jx_nc.js |ts >> /scripts/logs/jx_nc.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 # #临时增加红包雨
-# echo "28,29 20-23/1 * * * wget -O /scripts/jd_live_redrain.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_live_redrain.js " >> /scripts/docker/merged_list_file.sh
-# echo "30,31 20-23/1 * * * node /scripts/jd_live_redrain.js |ts >> /scripts/logs/jd_live_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "28,29 20-23/1 * * * wget -O /scripts/jd_live_redrain2.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_live_redrain2.js " >> /scripts/docker/merged_list_file.sh
+echo "30,31 20-23/1 * * * node /scripts/jd_live_redrain2.js |ts >> /scripts/logs/jd_live_redrain2.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 ##使用自定义shell下载并配置执行美妆
 wget -O /scripts/jd_mh.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_mh.js
