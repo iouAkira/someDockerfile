31547803405d42ff21d854815fca5d4140f36a68
Akira <e.akimoto.akira@gmail.com>
2021-01-18 22:08:31 +0800

    Update shell_script_mod.sh

--- jd_scripts/shell_script_mod.sh
+++ jd_scripts/shell_script_mod.sh
@@ -1,43 +1,42 @@
 #!/bin/sh
+#@shylocks仓库脚本
+function initShylocks() {
+    git clone https://github.com/shylocks/Loon.git /shylocks
+    npm install
+}
 
-##下面只是做一个示例
-##使用自定义shell下载并配置执行农场
-# wget -O /scripts/jx_nc.js https://raw.githubusercontent.com/whyour/hundun/master/quanx/jx_nc.js
-# echo -e >> /scripts/docker/merged_list_file.sh
-# echo "10 9,18 * * * node /scripts/jx_nc.js |ts >> /scripts/logs/jx_nc.log 2>&1" >> /scripts/docker/merged_list_file.sh
+ if [ ! -d "/shylocks/" ]; then
+    echo "未检查到shylocks仓库脚本，初始化下载相关脚本"
+    initShylocks
+else
+    echo "更新jkd脚本相关文件"
+    git -C /shylocks reset --hard
+    git -C /shylocks pull --rebase
+fi
 
-# #临时增加红包雨
-echo "58,59 18-20/1 * * * wget -O /scripts/jd_live_redrain2.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_live_redrain2.js " >> /scripts/docker/merged_list_file.sh
-echo "0,1 19-21/1 * * * node /scripts/jd_live_redrain2.js |ts >> /scripts/logs/jd_live_redrain2.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 ##使用自定义shell下载并配置执行美妆
-wget -O /scripts/jd_mh.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_mh.js
-echo -e >> /scripts/docker/merged_list_file.sh
-echo "10 8,9,10 * * * node /scripts/jd_mh.js |ts >> /scripts/logs/jd_mh.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "10 8,9,10 * * * node /shylocks/jd_mh.js |ts >> /scripts/logs/jd_mh.log 2>&1" >> /scripts/docker/merged_list_file.sh
 ##使用自定义shell下载并配置执行宝洁美发屋
-wget -O /scripts/jd_bj.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_bj.js
-echo -e >> /scripts/docker/merged_list_file.sh
-echo "1 8,9 14-31/1 1 * node /scripts/jd_bj.js |ts >> /scripts/logs/jd_bj.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "1 8,9 14-31/1 1 * node /shylocks/jd_bj.js |ts >> /scripts/logs/jd_bj.log 2>&1" >> /scripts/docker/merged_list_file.sh
 ##使用自定义shell下载并配置执行京东粉丝专享
-wget -O /scripts/jd_wechat_sign.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_wechat_sign.js
-echo -e >> /scripts/docker/merged_list_file.sh
-echo "1 7 * * * node /scripts/jd_wechat_sign.js |ts >> /scripts/logs/jd_wechat_sign.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "1 7 * * * node /shylocks/jd_wechat_sign.js |ts >> /scripts/logs/jd_wechat_sign.log 2>&1" >> /scripts/docker/merged_list_file.sh
 ##使用自定义shell下载并配置执行京东粉丝专享
-wget -O /scripts/jd_ms.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_ms.js
-echo -e >> /scripts/docker/merged_list_file.sh
-echo "1 7 * * * node /scripts/jd_ms.js |ts >> /scripts/logs/jd_ms.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "1 7 * * * node /shylocks/jd_ms.js |ts >> /scripts/logs/jd_ms.log 2>&1" >> /scripts/docker/merged_list_file.sh
 #神券京豆
-wget -O /scripts/jd_super_coupon.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_super_coupon.js
-echo -e >> /scripts/docker/merged_list_file.sh
-echo "1 7 13 1 * node /scripts/jd_super_coupon.js |ts >> /scripts/logs/jd_super_coupon.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "1 7 13 1 * node /shylocks/jd_super_coupon.js |ts >> /scripts/logs/jd_super_coupon.log 2>&1" >> /scripts/docker/merged_list_file.sh
 #神券京豆
-wget -O /scripts/jd_mh_super.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_mh_super.js
-echo -e >> /scripts/docker/merged_list_file.sh
-echo "10 20 15 1 * node /scripts/jd_mh_super.js |ts >> /scripts/logs/jd_mh_super.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "10 20 15 1 * node /shylocks/jd_mh_super.js |ts >> /scripts/logs/jd_mh_super.log 2>&1" >> /scripts/docker/merged_list_file.sh
 #工业爱消除
-wget -O /scripts/jd_gyec.js https://raw.githubusercontent.com/shylocks/Loon/main/jd_gyec.js
-echo -e >> /scripts/docker/merged_list_file.sh
-echo "30 * * * * node /scripts/jd_gyec.js |ts >> /scripts/logs/jd_gyec.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "30 * * * * node /shylocks/jd_gyec.js |ts >> /scripts/logs/jd_gyec.log 2>&1" >> /scripts/docker/merged_list_file.sh
+#工业爱消除
+echo "30 * * * * node /shylocks/jd_gyec.js |ts >> /scripts/logs/jd_gyec.log 2>&1" >> /scripts/docker/merged_list_file.sh
+#小鸽有礼
+echo "5 7 * * * node /shylocks/jd_gyec.js |ts >> /scripts/logs/jd_gyec.log 2>&1" >> /scripts/docker/merged_list_file.sh
+
+# #临时增加红包雨
+echo "58,59 18-20/1 * * * git -C /shylocks reset --hard && git -C /shylocks pull --rebase" >> /scripts/docker/merged_list_file.sh
+echo "0,1 19-21/1 * * * node /shylocks/jd_live_redrain2.js |ts >> /scripts/logs/jd_live_redrain2.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 #由于bookshop作者还没有增加互助码环境变量，就自己手动sed进去了
 sed -i "s/shareCodesArr = \[\]/shareCodesArr = \['a25442c9de1a47ddbe4cd3c4828bd8ea@aba172068b7a46e2b6cf89563b919053','ed23af1e5a5946b381266b2192f8d4a2@aba172068b7a46e2b6cf89563b919053','ed23af1e5a5946b381266b2192f8d4a2@a25442c9de1a47ddbe4cd3c4828bd8ea'\]/g" /scripts/jd_bookshop.js
