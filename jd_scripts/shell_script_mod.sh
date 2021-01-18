ef2ebd739e942018848d4b62d62575f38645961c
Akira <e.akimoto.akira@gmail.com>
2021-01-18 22:13:56 +0800

    Update shell_script_mod.sh

--- jd_scripts/shell_script_mod.sh
+++ jd_scripts/shell_script_mod.sh
@@ -28,7 +28,7 @@ echo "1 7 13 1 * node /shylocks/jd_super_coupon.js |ts >> /scripts/logs/jd_super
 #工业爱消除
 echo "30 * * * * node /shylocks/jd_gyec.js |ts >> /scripts/logs/jd_gyec.log 2>&1" >> /scripts/docker/merged_list_file.sh
 #小鸽有礼
-echo "5 7 * * * node /shylocks/jd_gyec.js |ts >> /scripts/logs/jd_gyec.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "5 7 * * * node /shylocks/jd_xg.js |ts >> /scripts/logs/jd_xg.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 # #临时增加红包雨
 echo "58,59 18-20/1 * * * git -C /shylocks reset --hard && git -C /shylocks pull --rebase" >> /scripts/docker/merged_list_file.sh
