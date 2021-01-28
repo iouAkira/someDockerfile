01e137e48904b0e984aaedb1096726511a6da275
Akira <e.akimoto.akira@gmail.com>
2021-01-28 13:38:45 +0800

    Update shell_script_mod.sh

--- jd_scripts/shell_script_mod.sh
+++ jd_scripts/shell_script_mod.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 #@shylocks仓库脚本
 function initShylocks() {
-    #git clone https://github.com/shylocks/Loon.git /shylocks
-    #npm install
-    echo "skip"
+#     git clone https://github.com/shylocks/Loon.git /shylocks
+    git clone https://github.com/Tartarus2014/Script.git /shylocks
 }
 
  if [ ! -d "/shylocks/" ]; then
@@ -16,15 +15,15 @@ else
     #npm install --loglevel error
 fi
 
-##复制两个文件
-#cp -f /shylocks/jd*.js /scripts/
-##加入 @shylocks 仓库的定时任务
-cat /shylocks/docker/crontab_list.sh >> /scripts/docker/merged_list_file.sh
+##复制文件
+cp -f /shylocks/jd*.js /scripts/
 
 # #临时增加红包雨
-echo "58,59 18-20/1 * * * git -C /shylocks reset --hard && git -C /shylocks pull --rebase" >> /scripts/docker/merged_list_file.sh
+echo "0 0,9,11,13,15,17,19,20,21,22,23 * * *  node /scripts/jd_live_redrain_offical.js|ts >> /scripts/logs/jd_live_redrain_offical.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "0 0,9,11,13,15,17,19,20,21,23 3,5,20-30/1 1,2 * node /scripts/jd_live_redrain_nian.js|ts >> /scripts/logs/jd_live_redrain_nian.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "30,31 12-23/1 * * * node /scripts/jd_live_redrain_half.js |ts >> /scripts/logs/jd_live_redrain_half.log 2>&1" >> /scripts/docker/merged_list_file.sh
 echo "0,1 19-21/1 * * * node /scripts/jd_live_redrain2.js |ts >> /scripts/logs/jd_live_redrain2.log 2>&1" >> /scripts/docker/merged_list_file.sh
-echo "30,31 20-23/1 19 1 * node /scripts/jd_live_redrain.js |ts >> /scripts/logs/jd_live_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "30,31 20-23/1 28 1 * node /scripts/jd_live_redrain.js |ts >> /scripts/logs/jd_live_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 #由于bookshop作者还没有增加互助码环境变量，就自己手动sed进去了
 sed -i "s/shareCodesArr = \[\]/shareCodesArr = \['a25442c9de1a47ddbe4cd3c4828bd8ea@aba172068b7a46e2b6cf89563b919053','ed23af1e5a5946b381266b2192f8d4a2@aba172068b7a46e2b6cf89563b919053','ed23af1e5a5946b381266b2192f8d4a2@a25442c9de1a47ddbe4cd3c4828bd8ea'\]/g" /scripts/jd_bookshop.js
