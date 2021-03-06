c7364720d2fd686ff57db39bb6c0b2ab3688fd21
Akira <e.akimoto.akira@gmail.com>
2021-03-06 21:22:45 +0800

    Update docker_entrypoint.sh

--- AutoSignMachine/docker_entrypoint.sh
+++ AutoSignMachine/docker_entrypoint.sh
@@ -3,34 +3,34 @@ set -e
 
 #获取配置的自定义参数
 if [ $1 ]; then
-    run_cmd=$1
-    if [ $CUST_SSH_KEY ] && [ $CUST_REPO_URL ];then
-        echo "配置仓库更新密钥"
-        mkdir -p /root/.ssh
-        echo -e ${CUST_SSH_KEY} > /root/.ssh/id_rsa  
-        chmod 600 /root/.ssh/id_rsa
-        ssh-keyscan github.com > /root/.ssh/known_hosts
-        echo "容器启动，拉取脚本仓库代码..."
-        git clone ${CUST_REPO_URL} /AutoSignMachine
-    else
-        echo "未配置脚本仓库更新KEY 或者 脚本仓库地址，容器将无法启动成功，请配置好相关参数在重新启动"
-    fi
+	run_cmd=$1
+	if [ $CUST_SSH_KEY ] && [ $CUST_REPO_URL ]; then
+		echo "配置仓库更新密钥"
+		mkdir -p /root/.ssh
+		echo -e ${CUST_SSH_KEY} >/root/.ssh/id_rsa
+		chmod 600 /root/.ssh/id_rsa
+		ssh-keyscan github.com >/root/.ssh/known_hosts
+		echo "容器启动，拉取脚本仓库代码..."
+		git clone ${CUST_REPO_URL} /AutoSignMachine
+	else
+		echo "未配置脚本仓库更新KEY 或者 脚本仓库地址，容器将无法启动成功，请配置好相关参数在重新启动"
+	fi
 fi
 
 if [ $TASK_SHELL_SCRIPT ]; then
-    wget -O /AutoSignMachine/task_shell_script.sh $TASK_SHELL_SCRIPT
+	wget -O /AutoSignMachine/task_shell_script.sh $TASK_SHELL_SCRIPT
 else
-    cd /pss
-    git pull origin master
-    cp pack_some_script/task_shell_script.sh /AutoSignMachine/task_shell_script.sh
+	cd /pss
+	git pull origin master
+	cp pack_some_script/task_shell_script.sh /AutoSignMachine/task_shell_script.sh
 fi
 echo "------------------------------------------------执行定时任务任务shell脚本------------------------------------------------"
-sh /AutoSignMachine/task_shell_script.sh
+sh /AutoSignMachine/task_shell_script.sh $run_cmd
 echo "--------------------------------------------------默认定时任务执行完成---------------------------------------------------"
 
 if [ $run_cmd ]; then
-    echo "启动crondtab定时任务主进程..."
-    crond -f
+	echo "启动crondtab定时任务主进程..."
+	crond -f
 else
-    echo "默认定时任务执行结束。"
+	echo "默认定时任务执行结束。"
 fi
