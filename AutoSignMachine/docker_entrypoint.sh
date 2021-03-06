949efe46dce6d75de3631cec3e07430da40ad30b
Akira <e.akimoto.akira@gmail.com>
2021-03-06 21:04:07 +0800

    Update docker_entrypoint.sh

--- AutoSignMachine/docker_entrypoint.sh
+++ AutoSignMachine/docker_entrypoint.sh
@@ -20,7 +20,9 @@ fi
 if [ $TASK_SHELL_SCRIPT ]; then
     wget -O /AutoSignMachine/task_shell_script.sh $TASK_SHELL_SCRIPT
 else
-    wget -O /AutoSignMachine/task_shell_script.sh https://raw.githubusercontent.com/iouAkira/someDockerfile/master/AutoSignMachine/task_shell_script.sh
+    cd /pss
+    git pull origin master
+    cp pack_some_script/task_shell_script.sh /AutoSignMachine/task_shell_script.sh
 fi
 echo "------------------------------------------------执行定时任务任务shell脚本------------------------------------------------"
 sh /AutoSignMachine/task_shell_script.sh
