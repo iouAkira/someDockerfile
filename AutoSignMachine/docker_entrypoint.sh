924e16778eac8f2a3b79822dc9ac5060dc9a3222
Akira <e.akimoto.akira@gmail.com>
2021-03-06 20:52:47 +0800

    Update docker_entrypoint.sh

--- AutoSignMachine/docker_entrypoint.sh
+++ AutoSignMachine/docker_entrypoint.sh
@@ -4,16 +4,18 @@ set -e
 #获取配置的自定义参数
 if [ $1 ]; then
     run_cmd=$1
+    if [ $CUST_SSH_KEY ] && [ $CUST_REPO_URL ];then
+        echo "配置仓库更新密钥"
+        mkdir -p /root/.ssh
+        echo -e ${CUST_SSH_KEY} > /root/.ssh/id_rsa  
+        chmod 600 /root/.ssh/id_rsa
+        ssh-keyscan github.com > /root/.ssh/known_hosts
+        echo "容器启动，拉取脚本仓库代码..."
+        git clone ${CUST_REPO_URL} /AutoSignMachine
+    else
+        echo "未配置脚本仓库更新KEY 或者 脚本仓库地址，容器将无法启动成功，请配置好相关参数在重新启动"
+    fi
 fi
-
-echo "更新仓库代码..."
-cd /AutoSignMachine
-git reset --hard
-echo "git pull拉取最新代码..."
-git -C /AutoSignMachine pull --rebase
-git checkout dev
-echo "npm install 安装最新依赖"
-npm install --loglevel error --prefix /AutoSignMachine
 
 if [ $TASK_SHELL_SCRIPT ]; then
     wget -O /AutoSignMachine/task_shell_script.sh $TASK_SHELL_SCRIPT
