e89cb8a232bcb960561653336f0c5d45343fec7f
Akira <e.akimoto.akira@gmail.com>
2021-03-07 12:42:21 +0800

    Update task_shell_script.sh

--- AutoSignMachine/task_shell_script.sh
+++ AutoSignMachine/task_shell_script.sh
@@ -45,7 +45,7 @@ fi
 if [ $ENABLE_UNICOM ]; then
 	if [ -f $envFile ]; then
 		cp -f $envFile /AutoSignMachine/config/.env
-		if [[ -f $UNICOM_JOB_CONFIG ]]; then
+		if [ $UNICOM_JOB_CONFIG ]; then
 			echo "找到联通细分任务配置故拆分，针对每个任务增加定时任务"
 			minute=0
 			hour=8
