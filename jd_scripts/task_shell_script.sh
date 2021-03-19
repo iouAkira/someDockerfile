c6ebf7917873a188bea1b7e64875d9155708d944
Akira <e.akimoto.akira@gmail.com>
2021-03-19 15:04:42 +0800

    Update task_shell_script.sh

--- jd_scripts/task_shell_script.sh
+++ jd_scripts/task_shell_script.sh
@@ -170,7 +170,8 @@ for scriptFile in $(ls | grep -E "jd_|z_" | tr "\n" " "); do
         cp $scriptFile /scripts
         echo "#$(sed -n "s/.*new Env('\(.*\)').*/\1/p" $scriptFile)($scriptFile)" >>$mergedListFile
         echo "$(sed -n "s/.*cronexpr=\"\(.*\)\".*/\1/p" $scriptFile) spnode /scripts/$scriptFile |ts >>/scripts/logs/$(echo $scriptFile | sed "s/.js/.log/g") 2>&1 &" >>$mergedListFile
-        if [ ! -n "$(crontab -l | grep "$scriptFile")" ]; then
+        chk=$(crontab -l | grep "$scriptFile")
+        if [ -n "$chk" ]; then
             echo "发现以前crontab里面不存在的任务，先跑为敬 $scriptFile"
             node /scripts/$scriptFile |ts >>/scripts/logs/$(echo $scriptFile | sed "s/.js/.log/g") 2>&1 &
         fi
