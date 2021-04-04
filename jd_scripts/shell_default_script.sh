2cb0fe2e98047d6015b02f82da65cd3729bce5d3
iouAkira <e.akimoto.akira@gmail.com>
2021-04-04 10:28:03 +0800

    无聊的调整

--- jd_scripts/shell_default_script.sh
+++ jd_scripts/shell_default_script.sh
@@ -11,20 +11,20 @@ first=\$1
 cmd=\$*
 echo \${cmd/\$1/}
 if [ \$1 == "conc" ]; then
-    for job in \$(cat \$COOKIES_LIST | grep -v "#" | paste -s -d ' '); do
+    for job in \$(cat \$COOKIE_LIST | grep -v "#" | paste -s -d ' '); do
         { export JD_COOKIE=\$job && node \${cmd/\$1/}
         }&
     done
 elif [ -n "\$(echo \$first | sed -n "/^[0-9]\+\$/p")" ]; then
     echo "\$(echo \$first | sed -n "/^[0-9]\+\$/p")"
-    { export JD_COOKIE=\$(sed -n "\${first}p" \$COOKIES_LIST) && node \${cmd/\$1/}
+    { export JD_COOKIE=\$(sed -n "\${first}p" \$COOKIE_LIST) && node \${cmd/\$1/}
     }&
-elif [ -n "\$(cat \$COOKIES_LIST  | grep "pt_pin=\$first")" ];then
-    echo "\$(cat \$COOKIES_LIST  | grep "pt_pin=\$first")"
-    { export JD_COOKIE=\$(cat \$COOKIES_LIST | grep "pt_pin=\$first") && node \${cmd/\$1/}
+elif [ -n "\$(cat \$COOKIE_LIST  | grep "pt_pin=\$first")" ];then
+    echo "\$(cat \$COOKIE_LIST  | grep "pt_pin=\$first")"
+    { export JD_COOKIE=\$(cat \$COOKIE_LIST | grep "pt_pin=\$first") && node \${cmd/\$1/}
     }&
 else
-    { export JD_COOKIE=\$(cat \$COOKIES_LIST | grep -v "#" | paste -s -d '&') && node \$*
+    { export JD_COOKIE=\$(cat \$COOKIE_LIST | grep -v "#" | paste -s -d '&') && node \$*
     }&
 fi
 EOF
