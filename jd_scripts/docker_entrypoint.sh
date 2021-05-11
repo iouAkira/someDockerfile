f192c04bdaee9a791dc482c2a99d4610c2b486a7
iouAkira <e.akimoto.akira@gmail.com>
2021-05-11 15:36:54 +0800

    update

--- jd_scripts/docker_entrypoint.sh
+++ jd_scripts/docker_entrypoint.sh
@@ -11,30 +11,21 @@ if [ "$1" ]; then
   initNodeEnv
   run_cmd=$1
 fi
+
 [ -f /scripts/package.json ] && before_package_json=$(cat /scripts/package.json)
 
-if [ -f "/scripts/logs/pull.lock" ]; then
-  echo "存在更新锁定文件，跳过git pull操作..."
+echo "执行repoSync"
+repoSync
+
+if [ ! -d /scripts/node_modules ]; then
+  echo "容器首次启动，执行npm install..."
+  npm install --loglevel error --prefix /scripts
 else
-  if [ -f "/data/id_rsa" ]; then
-      cat /data/id_rsa > /root/.ssh/id_rsa
-  fi
-  echo "设定远程仓库地址..."
-  cd /scripts
-  git remote set-url origin "$REPO_URL"
-  git reset --hard
-  echo "git pull拉取最新代码..."
-  git -C /scripts pull --rebase
-  if [ ! -d /scripts/node_modules ]; then
-    echo "容器首次启动，执行npm install..."
+  if [[ "${before_package_json}" != "$(cat /scripts/package.json)" ]]; then
+    echo "package.json有更新，执行npm install..."
     npm install --loglevel error --prefix /scripts
   else
-    if [[ "${before_package_json}" != "$(cat /scripts/package.json)" ]]; then
-      echo "package.json有更新，执行npm install..."
-      npm install --loglevel error --prefix /scripts
-    else
-      echo "package.json无变化，跳过npm install..."
-    fi
+    echo "package.json无变化，跳过npm install..."
   fi
 fi
 
