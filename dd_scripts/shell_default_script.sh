db0f4f0ee58a42aa2e788c9e4f37609495c74413
Akira <e.akimoto.akira@gmail.com>
2021-05-22 11:15:17 +0800

    Update shell_default_script.sh

--- dd_scripts/shell_default_script.sh
+++ dd_scripts/shell_default_script.sh
@@ -155,15 +155,15 @@ echo "附加额外特殊任务处理jd_crazy_joy_coin。。。"
 if [ ! "$CRZAY_JOY_COIN_ENABLE" ]; then
   echo "└──默认启用jd_crazy_joy_coin杀掉jd_crazy_joy_coin任务，并重启"
   eval $(ps -ef | grep "jd_crazy" | grep -v "grep" | awk '{print "kill "$1}')
-  echo '' >/scripts/logs/jd_crazy_joy_coin.log
-  spnode /scripts/jd_crazy_joy_coin.js | ts >>/scripts/logs/jd_crazy_joy_coin.log 2>&1 &
+  echo '' >/data/logs/jd_crazy_joy_coin.log
+  spnode /scripts/jd_crazy_joy_coin.js | ts >>/data/logs/jd_crazy_joy_coin.log 2>&1 &
   echo "└──默认jd_crazy_joy_coin重启完成"
 else
   if [ "$CRZAY_JOY_COIN_ENABLE" = "Y" ]; then
     echo "└──配置启用jd_crazy_joy_coin，杀掉jd_crazy_joy_coin任务，并重启"
     eval $(ps -ef | grep "jd_crazy" | grep -v "grep" | awk '{print "kill "$1}')
-    echo '' >/scripts/logs/jd_crazy_joy_coin.log
-    spnode /scripts/jd_crazy_joy_coin.js | ts >>/scripts/logs/jd_crazy_joy_coin.log 2>&1 &
+    echo '' >/data/logs/jd_crazy_joy_coin.log
+    spnode /scripts/jd_crazy_joy_coin.js | ts >>/data/logs/jd_crazy_joy_coin.log 2>&1 &
     echo "└──配置jd_crazy_joy_coin重启完成"
   else
     eval $(ps -ef | grep "jd_crazy" | grep -v "grep" | awk '{print "kill "$1}')
