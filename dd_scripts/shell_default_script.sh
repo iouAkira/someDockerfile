ec13f3a162d870f91dcf01ac83a09d2d076c2202
Akira <e.akimoto.akira@gmail.com>
2021-05-24 11:25:29 +0800

    Update shell_default_script.sh

--- dd_scripts/shell_default_script.sh
+++ dd_scripts/shell_default_script.sh
@@ -141,9 +141,9 @@ cat /jds/dd_scripts/docker_entrypoint.sh >/usr/local/bin/docker_entrypoint.sh
 echo "最后加载最新的附加功能定时任务文件..."
 echo "└──替换任务列表的node指令为spnode"
 sed -i "s/ node / spnode /g" $mergedListFile
-sed -i "/jd_carnivalcity/s/>>/>/g" $mergedListFile
+#sed -i "/jd_carnivalcity/s/>>/>/g" $mergedListFile
 echo "添加一些可以并发启动的脚本"
-sed -i "/\(jd_joy_reward.js\|jd_blueCoin.js\)/s/spnode/spnode conc/g" $mergedListFile
+sed -i "/\(jd_joy_reward.js\|jd_carnivalcity.js\|jd_blueCoin.js\)/s/spnode/spnode conc/g" $mergedListFile
 sed -i "s/\/scripts\/logs\//\/data\/logs\//g" $mergedListFile
 crontab $mergedListFile
 
