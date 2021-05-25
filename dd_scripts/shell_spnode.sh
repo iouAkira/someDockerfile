8d382fd6f379712ea6ad0b1dce8838b667053efb
iouAkira <e.akimoto.akira@gmail.com>
2021-05-25 22:27:22 +0800

    spnode 支持指定账号一次跑一遍所有任务

--- dd_scripts/shell_spnode.sh
+++ dd_scripts/shell_spnode.sh
@@ -6,18 +6,32 @@ cmd=$*
 #echo ${cmd/$1/}
 if [ $1 == "conc" ]; then
     for job in $(cat $COOKIE_LIST | grep -v "#" | paste -s -d ' '); do
-        { export JD_COOKIE=$job && node ${cmd/$1/}
-        }&
+        {
+            export JD_COOKIE=$job && node ${cmd/$1/}
+        } &
     done
 elif [ -n "$(echo $first | sed -n "/^[0-9]\+$/p")" ]; then
-    #echo "$(echo $first | sed -n "/^[0-9]\+$/p")"
-    { export JD_COOKIE=$(sed -n "${first}p" $COOKIE_LIST) && node ${cmd/$1/}
-    }&
-elif [ -n "$(cat $COOKIE_LIST  | grep "pt_pin=$first")" ];then
-    #echo "$(cat $COOKIE_LIST  | grep "pt_pin=$first")"
-    { export JD_COOKIE=$(cat $COOKIE_LIST | grep "pt_pin=$first") && node ${cmd/$1/}
-    }&
+    if [ $2 == "all" ]; then
+        for job in $(ls *_*.js | grep -v "JS_*\|JD_DailyBonus\|JD_extra_cookie\|USER_AGENTS" |sed "s/.js//g" | tr "\n" " "); do
+            export JD_COOKIE=$(cat $COOKIE_LIST | grep -v "#\|^$" | sed -n "${first}p") && node ${job} | tee -a "${LOGS_DIR}/${job}.log"
+        done
+    else
+        {
+            export JD_COOKIE=$(cat $COOKIE_LIST | grep -v "#\|^$" | sed -n "${first}p") && node ${cmd/$1/}
+        } &
+    fi
+elif [ -n "$(cat $COOKIE_LIST | grep "pt_pin=$first")" ]; then
+    if [ $2 == "all" ]; then
+        for job in $(ls *_*.js | grep -v "JS_*\|JD_DailyBonus\|JD_extra_cookie\|USER_AGENTS" |sed "s/.js//g" | tr "\n" " "); do
+            export JD_COOKIE=$(cat $COOKIE_LIST | grep -v "#\|^$" | sed -n "${first}p") && node ${job} | tee -a "${LOGS_DIR}/${job}.log"
+        done
+    else
+        {
+            export JD_COOKIE=$(cat $COOKIE_LIST | grep "pt_pin=$first") && node ${cmd/$1/}
+        } &
+    fi
 else
-    { export JD_COOKIE=$(cat $COOKIE_LIST | grep -v "#" | paste -s -d '&') && node $*
-    }&
+    {
+        export JD_COOKIE=$(cat $COOKIE_LIST | grep -v "#\|^$"| paste -s -d '&') && node $*
+    } &
 fi
