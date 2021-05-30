0c28e35ef67730d7b062d9cd62bc3c13929d47ef
Akira <e.akimoto.akira@gmail.com>
2021-05-30 11:21:48 +0800

    Update shell_mod_script.sh

--- dd_scripts/shell_mod_script.sh
+++ dd_scripts/shell_mod_script.sh
@@ -10,16 +10,16 @@ mergedListFile="/scripts/docker/merged_list_file.sh"
 echo "附加功能1，使用jds仓库的genCodeConf.list文件"
 cp /jds/dd_scripts/genCodeConf.list "$GEN_CODE_LIST"
 
-echo "附加功能2，拉取@monk-coder的dust仓库的代码，并增加相关任务"
-if [ ! -d "/monk/" ]; then
-    echo "未检查到monk-coder仓库脚本，初始化下载相关脚本..."
-    #   cp -rf /local_scripts/monk/ /monk
-    git clone https://github.com/monk-coder/dust /monk
-else
-    echo "更新monk-coder脚本相关文件..."
-    git -C /monk reset --hard
-    git -C /monk pull --rebase
-fi
+# echo "附加功能2，拉取@monk-coder的dust仓库的代码，并增加相关任务"
+# if [ ! -d "/monk/" ]; then
+#     echo "未检查到monk-coder仓库脚本，初始化下载相关脚本..."
+#     #   cp -rf /local_scripts/monk/ /monk
+#     git clone https://github.com/monk-coder/dust /monk
+# else
+#     echo "更新monk-coder脚本相关文件..."
+#     git -C /monk reset --hard
+#     git -C /monk pull --rebase
+# fi
 
 if [ -n "$(ls /monk/car/*_*.js)" ]; then
     cp -f /monk/car/*_*.js /scripts
