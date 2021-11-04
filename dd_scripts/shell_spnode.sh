a5a9f41a5eec6a3a230cc958cea3821469ea22a9
Akira <e.akimoto.akira@gmail.com>
2021-11-04 17:12:26 +0800

    update tips

--- dd_scripts/shell_spnode.sh
+++ dd_scripts/shell_spnode.sh
@@ -10,9 +10,10 @@ cmd=$*
 # 判断命令是否需要执行混淆后的js脚本
 if [ -n "$(echo $cmd | grep ".js_hx")" ]; then
     if [ $DEFAULT_EXEC_HX_SCRIPT == "Y" ]; then
+        echo "配置了 DEFAULT_EXEC_HX_SCRIPT=Y，混淆脚本执行命令继续..."
         cmd=$(echo $cmd | sed s/.js_hx/.js/g)
     else
-        echo '该仓库定时执行的为加密脚本，退出执行。如需启用请配置【export DEFAULT_EXEC_HX_SCRIPT="Y"】'
+        echo '执行的为混淆脚本，退出执行。如需启用请配置【export DEFAULT_EXEC_HX_SCRIPT="Y"】'
         exit 0
     fi
 fi
@@ -30,7 +31,7 @@ elif [ -n "$(echo $first | sed -n "/^[0-9]\+$/p")" ]; then
         done
     else
         {
-            echo "执行混淆后的js脚本命令为[node${cmd/$1/}]"
+            echo "执行的js脚本命令为[node${cmd/$1/}]"
             export JD_COOKIE=$(cat $COOKIE_LIST | grep -v "#\|^$" | sed -n "${first}p") && node ${cmd/$1/}
         } &
     fi
@@ -41,13 +42,13 @@ elif [ -n "$(cat $COOKIE_LIST | grep "pt_pin=$first")" ]; then
         done
     else
         {   
-            echo "执行混淆后的js脚本命令为[node${cmd/$1/}]"
+            echo "执行的js脚本命令为[node${cmd/$1/}]"
             export JD_COOKIE=$(cat $COOKIE_LIST | grep "pt_pin=$first") && node ${cmd/$1/}
         } &
     fi
 else
     {
-        echo "执行混淆后的js脚本命令为[node ${cmd}]"
+        echo "执行的js脚本命令为[node ${cmd}]"
         export JD_COOKIE=$(cat $COOKIE_LIST | grep -v "#\|^$" | paste -s -d '&') && node $cmd
     } &
 fi
