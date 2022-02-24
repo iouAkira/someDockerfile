5925f076a877aa4b8a04787621555a72ca5369de
Akira <e.akimoto.akira@gmail.com>
2022-02-24 21:03:08 +0800

    update

--- dd_scripts/shell_default_script.sh
+++ dd_scripts/shell_default_script.sh
@@ -217,7 +217,6 @@ if [ -n "$(ls /data/custom_scripts/*_*.js)" ]; then
     cp -f /data/custom_scripts/jd_*.js /scripts
     cd /data/custom_scripts/
     for scriptFile in $(ls *_*.js | tr "\n" " "); do
-        echo "dev $scriptFile"
         if [ -n "$(sed -n "s/.*cronexpr=\"\(.*\)\".*/\1/p" $scriptFile)" ]; then
           cp $scriptFile /scripts
           if [[ -z "$(cat $DD_CRON_FILE_PATH | grep $scriptFile)" && -z $1 ]]; then
